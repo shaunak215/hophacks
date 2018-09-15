091d3581f2fecd78d9409fabf285e6cd629c96b8
Andros Yang <androssdragonq.9@gmail.com>
2018-09-15 10:38:49 -0700

    Added support for multiple shelter websites

--- main.js
+++ main.js
@@ -16,26 +16,26 @@ var spawn = require('child_process').spawn;
 
 //Configuration constants
 const DARKNET_EXE = "C:\\Users\\MLH Admin\\darknet\\build\\darknet\\x64\\";
-const IMAGE_OUTPUT = "C:\\Users\\MLH Admin\\Desktop\\Proj\\";
 const NODEJS_DIR = __dirname;
 
 process.chdir(DARKNET_EXE);
 
-var shelter_capacity = {};
+var shelter_capacity = {
+	'shelter1': 0,
+	'shelter2': 0,
+	'shelter3': 12
+};
 
 router.post('/upload', function(req, res) {	
-	//Parse query params
-
-
 	//Extract some of the HTTP headers if they exist
 	var base64Data = req.rawBody.replace(/^data:image\/jpeg;base64,/, "");
 
 	//Write the file
-	fs.writeFile(IMAGE_OUTPUT + "out.jpg", base64Data, 'base64', function(err) {
+	fs.writeFile(NODEJS_DIR + "out.jpg", base64Data, 'base64', function(err) {
 		//HACK: Horribly hacky interfacing with Darknet
 		//Actually send the file to YOLO for processing
 		const ls = spawn(DARKNET_EXE + "darknet_no_gpu", ['detector', 'test', 'cfg/coco.data', 'cfg/yolov3.cfg', 
-			'yolov3.weights', IMAGE_OUTPUT + "out.jpg", "-dont_show"]);
+			'yolov3.weights', NODEJS_DIR + "out.jpg", "-dont_show"]);
 
 		let personCount = 0;
 
@@ -50,14 +50,39 @@ router.post('/upload', function(req, res) {
 
 		ls.on('close', (code) => {
 			console.log("There are " + personCount + " people");
+			//Parse query params
+			shelter_capacity[req.query.shelter];
+			res.send("Done");
 		});
 	});
 });
 
+const shelter_options = {
+	'shelter1': {
+		max: 4,
+		page: fs.readFileSync(NODEJS_DIR + '/views/.html').toString()
+	},
+	'shelter2': {
+		max: 8,
+		page: fs.readFileSync(NODEJS_DIR + '/views/.html').toString()
+	},
+	'shelter3': {
+		max: 12,
+		page: fs.readFileSync(NODEJS_DIR + '/views/.html').toString()
+	}}
 router.get('/shelter', function(req, res){
 	//Locate shelter
-	//Load the HTML file as a string
+	let capacity = shelter_capacity[req.query.shelter];
+	let maxCapacity = shelter_options[req.query.shelter].max;	
+
+	//Load file
+	let pageHTML = shelter_options[req.query.shelter].page;
+
 	//Findall replace
+	pageHTML = replaceAll(pageHTML, "$current$", "" + capacity);
+	pageHTML = replaceAll(pageHTML, "$max$", "" + maxCapacity);
+
+	res.send(pageHTML);
 });
 
 router.get('/', function(req, res){
@@ -74,3 +99,9 @@ router.use('/static', express.static(DARKNET_EXE))
 
 router.listen(3000);
 console.log("Server listening on port 3000");
+console.log("Darknet Dir: " + DARKNET_EXE);
+console.log("NodeJS Context: " + NODEJS_DIR);
+
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
+}
