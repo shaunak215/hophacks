1167397bcb05d2ecdd9d2270de4db87e19c79afb
Andros Yang <androssdragonq.9@gmail.com>
2018-09-15 03:08:54 -0700

    I'm sorry i added node_modules

---
+++ main.js
@@ -0,0 +1,68 @@
+var express = require('express')
+  , router = express()
+  , multer = require('multer')
+
+
+var rawBodySaver = function (req, res, buf, encoding) {
+  if (buf && buf.length) {
+    req.rawBody = buf.toString(encoding || 'utf8');
+  }
+}
+
+var bodyParser = require('body-parser');
+router.use(bodyParser.raw({limit: '50mb', verify: rawBodySaver, type: function () { return true } }));
+var fs = require("fs");
+var spawn = require('child_process').spawn;
+
+//Configuration constants
+const DARKNET_EXE = "C:\\Users\\MLH Admin\\darknet\\build\\darknet\\x64\\";
+const IMAGE_OUTPUT = "C:\\Users\\MLH Admin\\Desktop\\Proj\\";
+
+process.chdir(DARKNET_EXE);
+
+var shelter_capacity = {};
+
+router.post('/upload', function(req, res) {	
+	//Parse query params
+
+
+	//Extract some of the HTTP headers if they exist
+	var base64Data = req.rawBody.replace(/^data:image\/jpeg;base64,/, "");
+
+	//Write the file
+	fs.writeFile(IMAGE_OUTPUT + "out.jpg", base64Data, 'base64', function(err) {
+		//HACK: Horribly hacky interfacing with Darknet
+		//Actually send the file to YOLO for processing
+		const ls = spawn(DARKNET_EXE + "darknet_no_gpu", ['detector', 'test', 'cfg/coco.data', 'cfg/yolov3.cfg', 'yolov3.weights', IMAGE_OUTPUT + "out.jpg", "-dont_show"]);
+
+		let personCount = 0;
+
+		ls.stderr.on('data', (data) => {
+			//console.log(data.toString());
+		});
+
+		ls.stdout.on('data', (data) => {
+			//Extract the number of people from the output
+			personCount += (data.toString().match(new RegExp("person", "g")) || []).length;
+		});
+
+		ls.on('close', (code) => {
+			console.log("There are " + personCount + " people");
+		});
+	});
+});
+
+router.get('/shelter', function(req, res){
+	//Locate shelter
+	//Load the HTML file as a string
+	//Findall replace
+});
+
+router.get('/', function(req, res){
+	console.log("New connection");
+	res.sendFile('test.html');
+
+
+});
+
+router.listen(3000);
